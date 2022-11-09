182134b8769b53142d094fc467217b7a0b5a819c
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2022-11-09 16:39:48 +0100

    Added file upload service

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import UploadPage from "./pages/UploadPage";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/upload" element={<UploadPage />} />
         <Route path="/" element={<HomePage />} />
       </Routes>
     </div>
