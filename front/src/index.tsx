7d898b7080ba2b5cbb1f6e1fd1d6a1b88d1a8346
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2022-12-11 18:37:48 +0100

    Added register page and end to end test

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -13,12 +13,13 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000/",
+  uri: "http://backend:5000/",
 });
 
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
   const token = localStorage.getItem("token");
+  console.log("token from localstorage ", token);
   // return the headers to the context so httpLink can read them
   return {
     headers: {
