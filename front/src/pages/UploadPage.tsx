08523af1eccb26b87d950a13a99fe08040a5c320
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2023-01-30 15:05:40 +0100

    Added image view in upload component

--- front/src/pages/UploadPage.tsx
+++ front/src/pages/UploadPage.tsx
@@ -3,37 +3,57 @@ import axios from "axios";
 
 const UploadPage = () => {
   const [file, setFile] = useState<File>();
-  console.log("file", file);
+  const [imageURL, setImageURL] = useState<String>();
   return (
-    <form
-      onSubmit={async (event) => {
-        event.preventDefault();
-        if (file) {
-          const url = "http://localhost:8000/upload";
-          const formData = new FormData();
-          formData.append("file", file, file.name);
-          try {
-            const response = await axios.post(url, formData);
-            console.log(response);
-          } catch (err) {
-            console.log("error", err);
-          }
-        } else {
-          alert("select a file to upload");
-        }
-      }}
-    >
-      <h1>React File Upload</h1>
-      <input
-        type="file"
-        onChange={(e) => {
-          if (e.target.files) {
-            setFile(e.target.files[0]);
+    <div>
+      <form
+        onSubmit={async (event) => {
+          event.preventDefault();
+          if (file) {
+            const url = "http://localhost:8000/upload";
+            const formData = new FormData();
+            formData.append("file", file, file.name);
+            try {
+              const response = await axios.post(url, formData);
+              setImageURL(response.data.filename);
+            } catch (err) {
+              console.log("error", err);
+            }
+          } else {
+            alert("select a file to upload");
           }
         }}
-      />
-      <button type="submit">Upload</button>
-    </form>
+      >
+        <h1>React File Upload</h1>
+        <input
+          type="file"
+          onChange={(e) => {
+            if (e.target.files) {
+              setFile(e.target.files[0]);
+            }
+          }}
+        />
+        <button type="submit">Upload Image</button>
+      </form>
+      {imageURL ? (
+        <>
+          <br />
+          <img
+            width={"500"}
+            alt="uploadedImg"
+            src={"http://localhost:8000" + imageURL}
+          />
+          <br />
+        </>
+      ) : null}
+      <button
+        onClick={() => {
+          console.log("post this to backend: " + imageURL);
+        }}
+      >
+        Add new image
+      </button>
+    </div>
   );
 };
 
